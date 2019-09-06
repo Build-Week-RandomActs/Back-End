1eb5791914e338fac9bafd263fb6b1462e39392c
Lohrem <30306978+Lohrem@users.noreply.github.com>
2019-09-05 22:42:58 -0400

    added starter code for dbConfig.js

--- database/dbConfig.js
+++ database/dbConfig.js
@@ -1,1 +1,6 @@
 //db config belongs here. will need to add seeds and migrations to this folder.
+const knex = require('knex')
+
+const knexConfig = require('../knexfile.js')
+
+module.exports = knex(knexConfig.development)
