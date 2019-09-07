2fba11206dcda2101d74bb935d3548efaa715f2a
Lohrem <30306978+Lohrem@users.noreply.github.com>
2019-09-07 00:54:57 -0400

    added starter code for server.js

--- api/server.js
+++ api/server.js
@@ -1,1 +1,21 @@
 //server to routes
+const express = require('express')
+const cors = require('cors')
+const helmet = require('helmet')
+
+const actsRouter = require('../acts/acts-router.js')
+const authenticate = require('../authentication/restricted-middleware.js')
+const authRouter = require('../authentication/auth-router.js')
+const contactsRouter = require('../contacts/contacts-router.js')
+
+const server = express()
+
+server.use(helmet())
+server.use(cors())
+server.use(express.json())
+
+server.use('/api/acts', authenticate, actsRouter)
+server.use('/api/auth', authenticate, authRouter)
+server.use('/api/contacts', authenticate, contactsRouter)
+
+module.exports = server
